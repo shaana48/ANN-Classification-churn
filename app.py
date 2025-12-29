8a3d81dc4ac94543445100e3a770b931f99c58d6
Fathima Shahana N <fathimashahana9501@gmail.com>
2025-12-29 21:56:37 +0530

    Salary Prediction

--- app.py
+++ app.py
@@ -18,9 +18,8 @@ with open('onehot_encoder_geo.pkl', 'rb') as file:
 with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
-
 ## streamlit app
-st.title('Customer Churn Prediction')
+st.title('Estimated Salary Prediction')
 
 # User input
 geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
@@ -28,7 +27,7 @@ gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
 balance = st.number_input('Balance')
 credit_score = st.number_input('Credit Score')
-estimated_salary = st.number_input('Estimated Salary')
+exited = st.selectbox('Exited', [0, 1])
 tenure = st.slider('Tenure', 0, 10)
 num_of_products = st.slider('Number of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0, 1])
@@ -44,7 +43,7 @@ input_data = pd.DataFrame({
     'NumOfProducts': [num_of_products],
     'HasCrCard': [has_cr_card],
     'IsActiveMember': [is_active_member],
-    'EstimatedSalary': [estimated_salary]
+    'Exited': [exited]
 })
 
 # One-hot encode 'Geography'
@@ -57,15 +56,10 @@ input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis
 # Scale the input data
 input_data_scaled = scaler.transform(input_data)
 
-
 # Predict churn
 prediction = model.predict(input_data_scaled)
 prediction_proba = prediction[0][0]
 
-st.write(f'Churn Probability: {prediction_proba:.2f}')
-
-if prediction_proba > 0.5:
-    st.write('The customer is likely to churn.')
-else:
-    st.write('The customer is not likely to churn.')
+st.write(f'Predicted Estimated Salary: {prediction_proba:.2f}')
+ 
      
---
+++ streamlit_regression.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+import pandas as pd
+import pickle
+
+# Load the trained model
+model = tf.keras.models.load_model('regression_model.h5')
+
+# Load the encoders and scaler
+with open('label_encoder_gender.pkl', 'rb') as file:
+    label_encoder_gender = pickle.load(file)
+
+with open('onehot_encoder_geo.pkl', 'rb') as file:
+    onehot_encoder_geo = pickle.load(file)
+
+with open('scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
